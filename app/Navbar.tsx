c4a89178d2eb687b49cba303d30337550f23168b
Mamazorus <mauzenet@gmail.com>
2026-03-09 23:01:21 +0100

    design date

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -6,9 +6,22 @@ export default function Navbar() {
     <div className="bg-black text-white px-6 pt-6 pb-3">
       <div className="flex flex-col items-center gap-3">
         <Link href="./"><Image src="/img/rawsport_logo_blanc.svg" alt="logo Raw Studio" width={44} height={44} /></Link>
-        <p className="text-[12px] tracking-widest">
-          <span className="fade-pulse">XX</span>-04-2026
-        </p>
+        <div className="flex text-[10px] tracking-widest items-center justify-center gap-3">
+          <div className="flex flex-col items-center gap-0.5">
+            <p className="fade-pulse">XX</p>
+            <p className="opacity-50">DAY</p>
+          </div>
+          <p className="opacity-30 mb-3">—</p>
+          <div className="flex flex-col items-center gap-0.5">
+            <p>04</p>
+            <p className="opacity-50">MONTH</p>
+          </div>
+          <p className="opacity-30 mb-3">—</p>
+          <div className="flex flex-col items-center gap-0.5">
+            <p>2026</p>
+            <p className="opacity-50">YEAR</p>
+          </div>
+        </div>
       </div>
     </div>
   )
